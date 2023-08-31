e0bcf805af52c1d101ddefce62c44c574b2f5be1
Alex Flores <domain@me.com>
2023-08-31 10:46:25 -0400

    k8s things

--- private_dot_local/share/zsh/aliases/090_general.zsh
+++ private_dot_local/share/zsh/aliases/090_general.zsh
@@ -8,6 +8,12 @@ alias e="${EDITOR}"
 alias cm="chezmoi"
 alias reload="source ~/.zshrc"
 
-
 alias k=kubectl
+
 alias kg='k get'
+alias kgs='kg svc'
+alias kgp='kg pod'
+
+alias kga='kg --all-namespaces'
+alias kgas='kga svc'
+alias kgap='kga pods'
