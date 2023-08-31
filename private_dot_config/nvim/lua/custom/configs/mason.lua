6df9bdb2639156f4674e66cb234f3debd200e3ea
Alex Flores <domain@me.com>
2023-08-31 12:44:57 -0400

    nvim lsp stuff

---
+++ private_dot_config/nvim/lua/custom/configs/mason.lua
@@ -0,0 +1,30 @@
+local M = {}
+
+M.ensure_installed = {
+  "gopls",
+  "lua-language-server",
+  "prettier",
+  "cppcheck",
+  "csharpier",
+  "flake8",
+  "goimports_reviser",
+  "golangci_lint",
+  "prettier",
+  "selene",
+  "standardrb",
+  "staticcheck",
+  "stylua",
+  "yamllint",
+  "yq",
+  "clangd",
+  "csharp_ls",
+  "cssls",
+  "gopls",
+  "html",
+  "jedi_language_server",
+  "ruby-lsp",
+  "sourcekit",
+  "yamlls",
+}
+
+return M
