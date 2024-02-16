9c45c16612b187c06132f06cd59fd16456793b61
Alex Flores <domain@me.com>
2024-02-16 11:45:07 -0500

    keys

---
+++ private_dot_local/share/zsh/key-bindings.zsh
@@ -0,0 +1,40 @@
+# Make sure that the terminal is in application mode when zle is active, since
+# only then values from $terminfo are valid
+if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
+  function zle-line-init() {
+    echoti smkx
+  }
+  function zle-line-finish() {
+    echoti rmkx
+  }
+  zle -N zle-line-init
+  zle -N zle-line-finish
+fi
+
+# start typing + [Up-Arrow] - fuzzy find history forward
+if [[ "${terminfo[kcuu1]}" != "" ]]; then
+  autoload -U up-line-or-beginning-search
+  zle -N up-line-or-beginning-search
+  bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+fi
+
+# start typing + [Down-Arrow] - fuzzy find history backward
+if [[ "${terminfo[kcud1]}" != "" ]]; then
+  autoload -U down-line-or-beginning-search
+  zle -N down-line-or-beginning-search
+  bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
+fi
+
+# Filter through multiple ssh config files and hosts
+function get-ssh() {
+  # read the selected Host entry
+  IFS=: read host <<< $(grep '^Host [^*]' ${HOME}/.ssh/*config | sed 's/:Host//' | tr -d '"'| fzf)
+  # set the local buffer to the new command
+  LBUFFER="${LBUFFER} -F ${host}"
+  local ret=$?
+  zle reset-prompt
+  return $ret
+}
+zle     -N   get-ssh
+bindkey '^S' get-ssh
+
