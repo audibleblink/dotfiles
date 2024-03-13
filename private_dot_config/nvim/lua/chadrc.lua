fd5e93244f3e432c231ca20518d0c6622d9f1067
Alex Flores <domain@me.com>
2024-03-13 16:58:14 -0400

    (nvim) modify default term widths on floating term

--- private_dot_config/nvim/lua/chadrc.lua
+++ private_dot_config/nvim/lua/chadrc.lua
@@ -1,13 +1,22 @@
 local M = {}
 
 M.ui = {
-	theme = "bearded-arc",
-	tabufline = { enabled = false },
-	statusline = { separator_style = "arrow" },
-	nvdash = { load_on_startup = true },
-	hl_override = {
-		CursorLine = { bg = "black2" },
-	},
+  transparency = true,
+  theme = "bearded-arc",
+  tabufline = { enabled = false },
+  statusline = { separator_style = "round", },
+  hl_override = { CursorLine = { bg = "black2" }, },
+  term = {
+    sizes = { sp = 0.3, vsp = 0.2 },
+    float = {
+      relative = "editor",
+      row = 0.05,
+      col = 0.15,
+      width = 0.7,
+      height = 0.6,
+      border = "single",
+    },
+  },
 }
 
 return M
