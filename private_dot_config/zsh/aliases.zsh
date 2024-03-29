a07536c5d95999a91509ac5c38af1f9620a5d7aa
Alex Flores <domain@me.com>
2024-03-28 22:31:03 -0400

    (zsh) remove grep options

--- private_dot_config/zsh/aliases.zsh
+++ private_dot_config/zsh/aliases.zsh
@@ -59,37 +59,6 @@ alias gst='git status'
 alias gt='git tag'
 alias fml='e -O $( git diff --name-only | uniq )' # Open all merge conflicts
 # ]]]
-# Grep [[[
-# is x grep argument available?
-function grep-flag-available() {
-    echo | grep $1 "" >/dev/null 2>&1
-}
-
-GREP_OPTIONS=""
-
-# color grep results
-if grep-flag-available --color=auto; then
-    GREP_OPTIONS+=" --color=auto"
-fi
-
-# ignore VCS folders (if the necessary grep flags are available)
-VCS_FOLDERS="{.bzr,CVS,.git,.hg,.svn}"
-
-if grep-flag-available --exclude-dir=.cvs; then
-    GREP_OPTIONS+=" --exclude-dir=$VCS_FOLDERS"
-elif grep-flag-available --exclude=.cvs; then
-    GREP_OPTIONS+=" --exclude=$VCS_FOLDERS"
-fi
-
-# export grep settings
-alias grep="grep $GREP_OPTIONS"
-
-# clean up
-unset GREP_OPTIONS
-unset VCS_FOLDERS
-unfunction grep-flag-available
-
-# ]]]
 
 (( $+commands[tree] )) && alias tree='LS_COLORS="di=34:ln=35:so=32;40:ex=31" tree'
 
