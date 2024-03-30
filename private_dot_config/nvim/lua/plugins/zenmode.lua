effea55f19ef253d8b489ede47e2a0b9fad51340
Alex Flores <domain@me.com>
2024-03-29 22:10:41 -0400

    (nvim) configure zenmode and twilight

--- private_dot_config/nvim/lua/plugins/zenmode.lua
+++ private_dot_config/nvim/lua/plugins/zenmode.lua
@@ -1,6 +1,9 @@
 return {
 	"folke/zen-mode.nvim",
 	cmd = "ZenMode",
+	keys = {
+		{ "<leader>zn", "<cmd> ZenMode <CR>", desc = "Toggle ZenMode" },
+	},
 	opts = {
 		window = {
 			backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
@@ -8,17 +11,20 @@ return {
 			-- * an absolute number of cells when > 1
 			-- * a percentage of the width / height of the editor when <= 1
 			-- * a function that returns the width or the height
-			width = 0.7, -- width of the Zen window
-			height = 0.8, -- height of the Zen window
+			width = function()
+				return math.min(80, vim.o.columns * 0.75)
+			end,
+			height = 0.9, -- height of the Zen window
 			-- by default, no options are changed for the Zen window
 			-- uncomment any of the options below, or add other vim.wo options you want to apply
 			options = {
-				signcolumn = "yes", -- disable signcolumn
-				-- number = true, -- disable number column
-				-- relativenumber = true, -- disable relative numbers
+				signcolumn = "no", -- disable signcolumn
+				number = false, -- disable number column
+				relativenumber = false, -- disable relative numbers
 				cursorline = false, -- disable cursorline
 				cursorcolumn = false, -- disable cursor column
-				foldcolumn = "1", -- disable fold column
+				foldcolumn = "0", -- disable fold column
+				colorcolumn = "0",
 				-- list = false, -- disable whitespace characters
 			},
 		},
@@ -27,14 +33,14 @@ return {
 			-- comment the lines to not apply the options
 			options = {
 				enabled = true,
-				ruler = false, -- disables the ruler text in the cmd line area
-				showcmd = false, -- disables the command in the last line of the screen
+				ruler = true, -- disables the ruler text in the cmd line area
+				showcmd = true, -- disables the command in the last line of the screen
 				-- you may turn on/off statusline in zen mode by setting 'laststatus'
 				-- statusline will be shown only if 'laststatus' == 3
 				laststatus = 1, -- turn off the statusline in zen mode
 			},
 			twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
-			gitsigns = { enabled = false }, -- disables git signs
+			gitsigns = { enabled = true }, -- disables git signs
 			tmux = { enabled = true }, -- disables the tmux statusline
 			-- this will change the font size on kitty when in zen mode
 			-- to make this work, you need to set the following kitty options:
@@ -42,7 +48,7 @@ return {
 			-- - listen_on unix:/tmp/kitty
 			kitty = {
 				enabled = true,
-				font = "+8", -- font size increment
+				font = "+6", -- font size increment
 			},
 			-- this will change the font size on alacritty when in zen mode
 			-- requires  Alacritty Version 0.10.0 or higher
@@ -60,7 +66,9 @@ return {
 			},
 		},
 		-- callback where you can add custom code when the Zen window opens
-		on_open = function(win) end,
+		on_open = function(win)
+			vim.o.colorcolumn = "0"
+		end,
 		-- callback where you can add custom code when the Zen window closes
 		on_close = function() end,
 	},
