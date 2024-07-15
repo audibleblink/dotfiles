c1087b3abadd11e6ee04f30e4cfe7539dfff2963
Alex Flores <domain@me.com>
2024-07-15 07:57:57 -0500

    (tmux) adds tmux-float

--- private_dot_config/nvim/lua/plugins/copilot.lua
+++ private_dot_config/nvim/lua/plugins/copilot.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		vim.cmd([[
     imap <silent><script><expr> <C-;> copilot#Accept("\<CR>")
-    let g:copilot_no_tab_map = v:true
+    let g:copilot_no_tab_map = v:false
   ]])
 	end,
 }
