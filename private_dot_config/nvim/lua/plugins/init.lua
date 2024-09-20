38fd56e51a4444605c2402a1ae81d6da2ede2bb7
Alex Flores <domain@me.com>
2024-09-19 21:53:11 -0500

    (nvim) indentline config

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -6,6 +6,7 @@ return {
 	{ "justinmk/vim-sneak", lazy = false },
 	{ "jiangmiao/auto-pairs", lazy = false },
 	{ "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
+	{ "lukas-reineke/indent-blankline.nvim", enabled = false },
 	-- {
 	-- 	"mrcjkb/rustaceanvim",
 	-- 	version = "^4", -- Recommended
--- private_dot_config/nvim/lua/plugins/mini.indentscope.lua
+++ private_dot_config/nvim/lua/plugins/mini.indentscope.lua
@@ -2,5 +2,13 @@ return {
 	"echasnovski/mini.indentscope",
 	version = false,
 	lazy = false,
-	opts = { symbol = "│" },
+	opts = {
+		symbol = "│",
+		draw = {
+			priority = 2,
+			animation = function(s, n)
+				return s / n * 20
+			end,
+		},
+	},
 }
