0ca20ceda0a1cdbe52e73818dd66634bb96355ac
Alex Flores <domain@me.com>
2024-09-19 23:14:13 -0500

    (nvim) adds mini.animate

---
+++ private_dot_config/nvim/lua/plugins/mini.animate.lua
@@ -0,0 +1,25 @@
+return {
+	"echasnovski/mini.animate",
+	version = false,
+	lazy = false,
+	config = function()
+		local mini = require("mini.animate")
+		mini.setup({
+			scroll = {
+				timing = mini.gen_timing.exponential({
+					easing = "out",
+					duration = 8,
+					unit = "step",
+				}),
+			},
+			cursor = {
+				-- enable = false,
+				timing = mini.gen_timing.quartic({
+					easing = "out",
+					duration = 180,
+					unit = "total",
+				}),
+			},
+		})
+	end,
+}
