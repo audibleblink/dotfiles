b19eb0d17240e12704f56c496343573ff5122111
Alex Flores <domain@me.com>
2025-01-20 13:58:49 -0600

    (nvim) adds image plugin

---
+++ private_dot_config/nvim/lua/plugins/image.lua
@@ -0,0 +1,9 @@
+return {
+	"3rd/image.nvim",
+	build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+	ft = "markdown",
+	opts = {
+		tmux_show_only_in_active_window = true, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+		processor = "magick_cli", -- or "magick_cli"
+	},
+}
