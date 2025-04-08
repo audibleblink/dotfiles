3070f0fec6492daf5805e51c0f13edf7808f6aca
Alex Flores <domain@me.com>
2025-04-07 20:42:52 -0500

    (karabiner) disable menubar icon

--- private_dot_config/nvim/lua/mappings/lsp.lua
+++ private_dot_config/nvim/lua/mappings/lsp.lua
@@ -4,12 +4,32 @@ local map = vim.keymap.set
 local vim = vim
 
 -- Navigation
-map('n', 'gr', vim.lsp.buf.references, { desc = "Show References" } )
-map('n', 'gd', vim.lsp.buf.definition, { desc = "Go to Definition" } )
-map('n', 'ga', vim.lsp.buf.rename, { desc = "Rename Symbol" } )
+map('n', 'gr', vim.lsp.buf.references, { desc = "Show References" })
+map('n', 'gd', vim.lsp.buf.definition, { desc = "Go to Definition" })
+map('n', 'ga', vim.lsp.buf.rename, { desc = "Rename Symbol" })
 
 -- Diagnostics
 map("n", "<leader>lf", vim.diagnostic.open_float, { desc = "Lsp floating diagnostics" })
 map("n", "[d", vim.diagnostic.goto_prev, { desc = "Lsp prev diagnostic" })
 map("n", "]d", vim.diagnostic.goto_next, { desc = "Lsp next diagnostic" })
 map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Lsp diagnostic loclist" })
+
+-- Log level
+vim.lsp.set_log_level(vim.log.levels.WARN)
+
+-- Diagnostics
+vim.diagnostic.config({
+	signs = { priority = 9999 },
+	underline = true,
+	update_in_insert = false, -- false so diags are updated on InsertLeave
+	virtual_text = { current_line = true, severity = { min = "INFO", max = "WARN" } },
+	virtual_lines = { current_line = true, severity = { min = "ERROR" } },
+	severity_sort = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = true,
+		header = "",
+	},
+})
