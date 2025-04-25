c4b3b6230f3cf8e8ea1c555ebb08c0b83687c392
Alex Flores <domain@me.com>
2025-04-24 20:13:29 -0500

    (tmux) better battery

--- private_dot_config/tmux/functions/executable_battery.sh
+++ private_dot_config/tmux/functions/executable_battery.sh
@@ -1,33 +1,63 @@
 #!/bin/bash
 
-HEART='󰣐 '
-IHEART='󱢠 '
-os=`uname`
+FULL_HEART='󰣐 '
+HALF_HEART='󰛞 '
+EMPTY_HEART='󱢠 '
 
-if [[ `uname` == 'Linux' ]]; then
-  current_charge=$(acpi | cut -d ' ' -f 4 | sed 's/[^0-9].*//')
-  total_charge=100
+os=$(uname)
+
+# Check if argument is provided for testing
+if [[ $# -eq 1 && "$1" =~ ^[0-9]+$ ]]; then
+  percentage=$1
+elif [[ $(uname) == 'Linux' ]]; then
+  percentage=$(acpi | cut -d ' ' -f 4 | sed 's/[^0-9].*//')
 else
-  battery_info=`ioreg -rc AppleSmartBattery`
+  battery_info=$(ioreg -rc AppleSmartBattery)
   current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
   total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
+  percentage=$((current_charge * 100 / total_charge))
 fi
 
-# checks if this is desktop or laptop
-if [[ "${os}" == 'Darwin' || "$(ls -A /sys/class/power_supply/)" ]]; then
-  charged_slots=$(echo "(($current_charge/$total_charge)*5)+1" | bc -l | cut -d '.' -f 1)
-  if [[ $charged_slots -gt 5 ]]; then
-    charged_slots=5
+# naively checks if this is desktop or laptop
+if [[ "${os}" == 'Darwin' || "$(ls -A /sys/class/power_supply/ 2>/dev/null)" ]]; then
+  
+  if [[ $percentage -lt 10 ]]; then
+    # 0-9%: Show 5 red empty hearts
+    output="#[fg=colour9]$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART"
+  elif [[ $percentage -lt 20 ]]; then
+    # 10-19%: Show a half heart
+    output="#[fg=colour9]$HALF_HEART#[fg=white]$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART"
+  elif [[ $percentage -lt 30 ]]; then
+    # 20-29%: Show one full heart 
+    output="#[fg=colour9]$FULL_HEART#[fg=white]$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART"
+  elif [[ $percentage -lt 40 ]]; then
+    # 30-39%: Show one full heart and one half heart
+    output="#[fg=colour9]$FULL_HEART$HALF_HEART#[fg=white]$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART"
+  elif [[ $percentage -lt 50 ]]; then
+    # 40-49%: Show two full hearts
+    output="#[fg=colour9]$FULL_HEART$FULL_HEART#[fg=white]$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART"
+  elif [[ $percentage -lt 60 ]]; then
+    # 50-59%: Show two full hearts and one half heart
+    output="#[fg=colour9]$FULL_HEART$FULL_HEART$HALF_HEART#[fg=white]$EMPTY_HEART$EMPTY_HEART"
+  elif [[ $percentage -lt 70 ]]; then
+    # 60-69%: Show three full hearts
+    output="#[fg=colour9]$FULL_HEART$FULL_HEART$FULL_HEART#[fg=white]$EMPTY_HEART$EMPTY_HEART"
+  elif [[ $percentage -lt 80 ]]; then
+    # 70-79%: Show three full hearts and one half heart
+    output="#[fg=colour9]$FULL_HEART$FULL_HEART$FULL_HEART$HALF_HEART#[fg=white]$EMPTY_HEART"
+  elif [[ $percentage -lt 90 ]]; then
+    # 80-89%: Show four full hearts
+    output="#[fg=colour9]$FULL_HEART$FULL_HEART$FULL_HEART$FULL_HEART#[fg=white]$EMPTY_HEART"
+  elif [[ $percentage -lt 95 ]]; then
+    # 90-95%: Show four full hearts and one half heart
+    output="#[fg=colour9]$FULL_HEART$FULL_HEART$FULL_HEART$FULL_HEART$HALF_HEART#[fg=white]"
+  else
+    # 95-100%: Show five full hearts
+    output="#[fg=colour9]$FULL_HEART$FULL_HEART$FULL_HEART$FULL_HEART$FULL_HEART"
   fi
 else
-  # were on a desktop, show 5 hearts
-  charged_slots=5
+  # We're on a desktop, show 5 full hearts
+  output="#[fg=colour9]$FULL_HEART$FULL_HEART$FULL_HEART$FULL_HEART$FULL_HEART"
 fi
 
-printf '#[fg=colour9]'
-for i in `seq 1 $charged_slots`; do printf "$HEART"; done
-
-if [[ $charged_slots -lt 5 ]]; then
-  printf '#[fg=white]'
-  for i in `seq 1 $(echo "5-$charged_slots" | bc)`; do printf "$IHEART"; done
-fi
+printf "%s" "$output"
