9bf8bfeeb03aa2634ee715aa44744565c1eee3ae
Alex Flores <domain@me.com>
2025-04-24 20:32:31 -0500

    (tmux) mathy battery

--- private_dot_config/tmux/functions/executable_battery.sh
+++ private_dot_config/tmux/functions/executable_battery.sh
@@ -11,53 +11,60 @@ if [[ $# -eq 1 && "$1" =~ ^[0-9]+$ ]]; then
   percentage=$1
 elif [[ $(uname) == 'Linux' ]]; then
   percentage=$(acpi | cut -d ' ' -f 4 | sed 's/[^0-9].*//')
-else
+
   battery_info=$(ioreg -rc AppleSmartBattery)
   current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
   total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
   percentage=$((current_charge * 100 / total_charge))
-fi
+
 
 # naively checks if this is desktop or laptop
 if [[ "${os}" == 'Darwin' || "$(ls -A /sys/class/power_supply/ 2>/dev/null)" ]]; then
-  
+  # Fix special cases first
   if [[ $percentage -lt 10 ]]; then
     # 0-9%: Show 5 red empty hearts
     output="#[fg=colour9]$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART"
-  elif [[ $percentage -lt 20 ]]; then
-    # 10-19%: Show a half heart
-    output="#[fg=colour9]$HALF_HEART#[fg=white]$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART"
-  elif [[ $percentage -lt 30 ]]; then
-    # 20-29%: Show one full heart 
-    output="#[fg=colour9]$FULL_HEART#[fg=white]$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART"
-  elif [[ $percentage -lt 40 ]]; then
-    # 30-39%: Show one full heart and one half heart
-    output="#[fg=colour9]$FULL_HEART$HALF_HEART#[fg=white]$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART"
-  elif [[ $percentage -lt 50 ]]; then
-    # 40-49%: Show two full hearts
-    output="#[fg=colour9]$FULL_HEART$FULL_HEART#[fg=white]$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART"
-  elif [[ $percentage -lt 60 ]]; then
-    # 50-59%: Show two full hearts and one half heart
-    output="#[fg=colour9]$FULL_HEART$FULL_HEART$HALF_HEART#[fg=white]$EMPTY_HEART$EMPTY_HEART"
-  elif [[ $percentage -lt 70 ]]; then
-    # 60-69%: Show three full hearts
-    output="#[fg=colour9]$FULL_HEART$FULL_HEART$FULL_HEART#[fg=white]$EMPTY_HEART$EMPTY_HEART"
-  elif [[ $percentage -lt 80 ]]; then
-    # 70-79%: Show three full hearts and one half heart
-    output="#[fg=colour9]$FULL_HEART$FULL_HEART$FULL_HEART$HALF_HEART#[fg=white]$EMPTY_HEART"
-  elif [[ $percentage -lt 90 ]]; then
-    # 80-89%: Show four full hearts
-    output="#[fg=colour9]$FULL_HEART$FULL_HEART$FULL_HEART$FULL_HEART#[fg=white]$EMPTY_HEART"
-  elif [[ $percentage -lt 95 ]]; then
-    # 90-95%: Show four full hearts and one half heart
-    output="#[fg=colour9]$FULL_HEART$FULL_HEART$FULL_HEART$FULL_HEART$HALF_HEART"
-  else
+  elif [[ $percentage -ge 95 ]]; then
     # 95-100%: Show five full hearts
     output="#[fg=colour9]$FULL_HEART$FULL_HEART$FULL_HEART$FULL_HEART$FULL_HEART"
+  else
+    # Calculate number of full hearts (integer division by 20)
+    full_hearts=$(( percentage / 20 ))
+
+    # Check if we need a half heart (remainder between 10-19)
+    remainder=$(( percentage % 20 ))
+    has_half_heart=0
+    if [[ $remainder -ge 10 ]]; then
+      has_half_heart=1
+    fi
+
+    # Calculate empty hearts
+    empty_hearts=$(( 5 - full_hearts - has_half_heart ))
+
+    # Build output string
+    output="#[fg=colour9]"
+
+    # Add full hearts
+    for (( i=0; i<full_hearts; i++ )); do
+      output+="$FULL_HEART"
+    done
+
+    # Add half heart if needed
+    if [[ $has_half_heart -eq 1 ]]; then
+      output+="$HALF_HEART"
+    fi
+
+    # Add empty hearts if needed
+    if [[ $empty_hearts -gt 0 ]]; then
+      output+="#[fg=white]"
+      for (( i=0; i<empty_hearts; i++ )); do
+        output+="$EMPTY_HEART"
+      done
+    fi
   fi
-else
+
   # We're on a desktop, show 5 full hearts
   output="#[fg=colour9]$FULL_HEART$FULL_HEART$FULL_HEART$FULL_HEART$FULL_HEART"
-fi
+
 
 printf "%s" "$output"
