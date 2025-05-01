e37e3a4157e2ac45a293690f8dd7a678a088ed1b
Alex Flores <domain@me.com>
2025-05-01 14:30:45 -0500

    (sketchy) switch again, this time w aerospace

---
+++ private_dot_config/sketchybar/plugins/executable_battery.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+source "$CONFIG_DIR/colors.sh"
+
+FULL_HEART='󰣐 '
+HALF_HEART='󰛞 '
+EMPTY_HEART='󱢠 '
+
+os=$(uname)
+
+battery_info=$(ioreg -rc AppleSmartBattery)
+current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
+total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
+percentage=$((current_charge * 100 / total_charge))
+
+# naively checks if this is desktop or laptop
+# Fix special cases first
+if [[ $percentage -lt 6 ]]; then
+  # 0-5%: Show 5 red empty hearts
+  output="$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART$EMPTY_HEART"
+elif [[ $percentage -ge 95 ]]; then
+  # 95-100%: Show five full hearts
+  output="$FULL_HEART$FULL_HEART$FULL_HEART$FULL_HEART$FULL_HEART"
+else
+  # Calculate number of full hearts (integer division by 20)
+  full_hearts=$(( percentage / 20 ))
+
+  # Check if we need a half heart (remainder between 10-19)
+  remainder=$(( percentage % 20 ))
+  has_half_heart=0
+  if [[ $remainder -ge 10 ]]; then
+    has_half_heart=1
+  fi
+
+  # Calculate empty hearts
+  empty_hearts=$(( 5 - full_hearts - has_half_heart ))
+
+  # Build output string
+  output=""
+
+  # Add full hearts
+  for (( i=0; i<full_hearts; i++ )); do
+    output+="$FULL_HEART"
+  done
+
+  # Add half heart if needed
+  if [[ $has_half_heart -eq 1 ]]; then
+    output+="$HALF_HEART"
+  fi
+
+  # Add empty hearts if needed
+  if [[ $empty_hearts -gt 0 ]]; then
+    output+=""
+    for (( i=0; i<empty_hearts; i++ )); do
+      output+="$EMPTY_HEART"
+    done
+  fi
+fi
+
+
+# The item invoking this script (name $NAME) will get its icon and label
+# updated with the current battery status
+sketchybar --set "$NAME" icon="${output}" label="${percentage}%" icon.color=$RED
