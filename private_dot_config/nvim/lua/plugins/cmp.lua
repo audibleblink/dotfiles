f3874135cc297759833407211ce1b53001047047
Alex Flores <domain@me.com>
2025-05-27 14:43:57 -0500

    (nvim) fix cmp and conform race/deps

--- private_dot_config/nvim/lua/plugins/cmp.lua
+++ private_dot_config/nvim/lua/plugins/cmp.lua
@@ -1,67 +1,48 @@
-local cmp = require("cmp")
--- local lspkind = require "lspkind"
---
-cmp.event:on("menu_opened", function()
-	vim.b.copilot_suggestion_hidden = true
-end)
-
 return {
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
-	opts = {
-		completion = { completeopt = "menu,menuone" },
-		preselect = cmp.PreselectMode.None,
-		snippet = {
-			expand = function(args)
-				require("luasnip").lsp_expand(args.body)
-			end,
-		},
+	config = function()
+		local cmp = require("cmp")
+		cmp.event:on("menu_opened", function()
+			vim.b.copilot_suggestion_hidden = true
+		end)
 
-		mapping = {
-			["<CR>"] = cmp.config.disable,
-			["<C-p>"] = cmp.mapping.select_prev_item(),
-			["<C-n>"] = cmp.mapping.select_next_item(),
-			["<C-d>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-e>"] = cmp.mapping.close(),
-			["<C-Space>"] = cmp.mapping.complete(),
+		cmp.setup({
+			completion = { completeopt = "menu,menuone" },
+			preselect = cmp.PreselectMode.None,
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
 
-			["<C-l>"] = cmp.mapping.confirm({
-				behavior = cmp.ConfirmBehavior.Insert,
-				select = false,
-			}),
-		},
+			mapping = {
+				["<CR>"] = cmp.config.disable,
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-e>"] = cmp.mapping.close(),
+				["<C-Space>"] = cmp.mapping.complete(),
 
-		sources = {
-			{ name = "nvim_lsp" },
-			{ name = "copilot" },
-			{ name = "luasnip" },
-			{ name = "buffer" },
-			{ name = "nvim_lua" },
-			{ name = "path" },
-		},
+				["<C-l>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Insert,
+					select = false,
+				}),
+			},
 
-		-- formatting = {
-		-- 	format = lspkind.cmp_format({
-		-- 		mode = "symbol",
-		-- 		max_width = 50,
-		-- 		symbol_map = { Copilot = "" }
-		-- 	})
-		-- }
-	}
-,
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "copilot" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "nvim_lua" },
+				{ name = "path" },
+			}
+		})
+	end,
+
 	dependencies = {
-		{
-			-- snippet plugin
-			"L3MON4D3/LuaSnip",
-			dependencies = "rafamadriz/friendly-snippets",
-			opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-			config = function(_, opts)
-				require("luasnip").config.set_config(opts)
-				require("nvchad.configs.luasnip")
-			end,
-		},
-
 		-- autopairing of (){}[] etc
 		{
 			"windwp/nvim-autopairs",
--- private_dot_config/nvim/lua/plugins/conform.lua
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -1,11 +1,12 @@
 return {
 	"stevearc/conform.nvim",
-	opts = {},
-	keys = {
-		{ "<leader>p", require("conform").format, desc = "[Conform] Format file" },
-	},
+	-- keys = {
+	-- 	{ "<leader>p", require("conform").format, desc = "[Conform] Format file" },
+	-- },
 	config = function()
+
 		require("conform").setup({
+
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "ruff", "isort", "black" },
@@ -17,8 +18,9 @@ return {
 				-- These options will be passed to conform.format()
 				timeout_ms = 500,
 				lsp_format = "fallback",
-			},
+			}
 		})
+
 		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-	end,
+	end
 }
