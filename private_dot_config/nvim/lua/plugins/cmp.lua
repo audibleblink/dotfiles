b1acb9a5e3093599a6fda64969ea805cc32a0a6d
Alex Flores <domain@me.com>
2025-06-09 17:04:52 -0500

    cluade

--- private_dot_config/nvim/lua/plugins/cmp.lua
+++ private_dot_config/nvim/lua/plugins/cmp.lua
@@ -65,8 +65,6 @@ return {
 			},
 			config = function(_, opts)
 				require("nvim-autopairs").setup(opts)
-
-				-- setup cmp for autopairs
 				local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 				require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
 			end,
@@ -78,6 +76,16 @@ return {
 				local cmp = require("cmp")
 				cmp.setup.cmdline(':', {
 					mapping = cmp.mapping.preset.cmdline(),
+					sources = cmp.config.sources({
+						{ name = 'path' }
+					}, {
+						{
+							name = 'cmdline',
+							option = {
+								ignore_cmds = { 'Man', '!' }
+							}
+						}
+					})
 				})
 			end
 		},
