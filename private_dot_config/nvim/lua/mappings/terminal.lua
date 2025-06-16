af4dbf29da6eff98aef6844cab7b3e2fca3568c1
Alex Flores <domain@me.com>
2025-06-16 16:44:09 -0500

    (nvim): dry up terminal command logic

--- private_dot_config/nvim/lua/mappings/terminal.lua
+++ private_dot_config/nvim/lua/mappings/terminal.lua
@@ -18,17 +18,26 @@ map(modes, "<leader>li", function()
 end, { desc = "Terminal Toggle Floating term" })
 
 local last_cmd = ""
+local function run_in_terminal(cmd, opts)
+	opts = opts or {}
+	local direction = opts.direction or "normal"
+	if direction == "vsplit" then
+		vim.cmd("vsplit")
+	elseif direction == "hsplit" then
+		vim.cmd("split")
+	elseif direction == "float" then
+		-- Implement floating window logic if desired
+	end
+	local term_buf = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_win_set_buf(0, term_buf)
+	vim.fn.termopen(cmd, opts.termopen_opts or {})
+end
+
 map(modes, "<leader>lr", function()
 	local cmd = vim.fn.input("Command to run: ", last_cmd)
 	if cmd and cmd ~= "" then
 		last_cmd = cmd
-		local term_buf = vim.api.nvim_create_buf(false, true)
-		vim.cmd("buffer " .. term_buf)
-		vim.fn.termopen(cmd, {
-			on_exit = function()
-				require("mini.bufremove").unshow()
-			end,
-		})
+		run_in_terminal(cmd, { direction = "normal" })
 	end
 end, { desc = "Run user command in terminal" })
 
@@ -36,20 +45,18 @@ map(modes, "<leader>lsv", function()
 	local cmd = vim.fn.input("Command to run (vs): ", last_cmd)
 	if cmd and cmd ~= "" then
 		last_cmd = cmd
-		vim.cmd("vsplit")
-		local term_buf = vim.api.nvim_create_buf(false, true)
-		vim.api.nvim_win_set_buf(0, term_buf)
-		vim.fn.termopen(cmd)
+		run_in_terminal(cmd, { direction = "vsplit" })
 	end
 end, { desc = "Run user command in vertical split terminal" })
 
 map(modes, "<leader>gc", function()
-	local term_buf = vim.api.nvim_create_buf(false, true)
-	vim.cmd("buffer " .. term_buf)
-	vim.fn.termopen("git commit -v", {
-		on_exit = function()
-			require("mini.bufremove").unshow()
-		end,
+	run_in_terminal("git commit -v", {
+		direction = "normal",
+		termopen_opts = {
+			on_exit = function()
+				require("mini.bufremove").unshow()
+			end,
+		},
 	})
 end, { desc = "Git Commit" })
 
