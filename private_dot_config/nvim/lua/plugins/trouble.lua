97e380351da2d177b49537dc5080b7a74a253c62
Alex Flores <domain@me.com>
2025-07-22 09:32:12 -0500

    (nvim) keymap symbols -> M-n

--- private_dot_config/nvim/lua/plugins/trouble.lua
+++ private_dot_config/nvim/lua/plugins/trouble.lua
@@ -3,8 +3,6 @@ return {
 	"folke/trouble.nvim",
 	cmd = "Trouble",
 	keys = {
-		{ "<leader>x" },
-		{ "<leader>o" },
 		{
 			"<leader>xx",
 			"<cmd>Trouble diagnostics toggle<cr>",
@@ -14,11 +12,6 @@ return {
 			"<leader>xX",
 			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
 			desc = "Buffer Diagnostics (Trouble)",
-		},
-		{
-			"<leader>xl",
-			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-			desc = "LSP Definitions / references / ... (Trouble)",
 		},
 	},
 	opts = {
@@ -76,7 +69,7 @@ return {
 		local trouble = require("trouble")
 		trouble.setup(opts)
 
-		vim.keymap.set("n", "<leader>o", function()
+		vim.keymap.set("n", "<M-n>", function()
 			trouble.toggle({ mode = "symbols" })
 		end, { desc = "Symbols Outline" })
 
