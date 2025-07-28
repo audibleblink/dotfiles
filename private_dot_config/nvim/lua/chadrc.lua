ac519411b6bafe90ed3475bb1d09fc6a61fcc23f
Alex Flores <domain@me.com>
2025-07-27 22:15:14 -0500

    (nvim) better path in statusbar

--- private_dot_config/nvim/lua/chadrc.lua
+++ private_dot_config/nvim/lua/chadrc.lua
@@ -15,12 +15,19 @@ M.ui = {
 	statusline = {
 		-- theme = "default",
 		separator_style = "round",
-		order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cursor", "cwd", "f" },
+		order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cursor", "cwd", "abc" },
 		modules = {
-			-- abc = function()
-			-- 	return "hi"
-			-- end,
-			f = "%f",
+			abc = function()
+				local buf_path = vim.api.nvim_buf_get_name(0)
+				local root = vim.fs.root(buf_path, { ".git", "HEAD" })
+				if not root then
+					return vim.fn.getcwd()
+				end
+				local relative_path = buf_path:sub(#root + 1)
+				local dir_path = vim.fs.dirname(relative_path)
+				dir_path = dir_path:gsub("^/", "")
+				return dir_path
+			end,
 		},
 	},
 }
