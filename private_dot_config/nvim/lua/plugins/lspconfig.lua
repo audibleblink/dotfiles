dafcb528c56ec40ecccd8a8144ade04b50f5fed1
Alex Flores <domain@me.com>
2025-08-13 09:27:55 -0500

    (nvim) mason - re-enable fetching registry updates

--- private_dot_config/nvim/lua/plugins/lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,30 +1,30 @@
-local servers = {
-	"basedpyright",
-	"denols",
-	"gopls",
-	"lua_ls",
-	"markdown_oxide",
-	"ruff",
-	"rust_analyzer",
-	"tinymist",
-	"yamlls",
-	"zls",
-}
-
+-- new version just places lsp configs in vim's $rtp/lsp. nice.
+-- use neovim's builtin lsp api to load
 return {
 	"neovim/nvim-lspconfig",
 	enabled = not vim.g.vscode,
 	event = { "BufReadPre", "BufNewFile" },
-	dependencies = { "mason-org/mason.nvim", opts = { ui = { check_outdated_packages_on_open = false } } },
+	dependencies = "mason-org/mason.nvim", -- not really, but nice to have in one file
 
 	config = function()
-		vim.lsp.enable(servers)
+		vim.lsp.enable({
+			"basedpyright",
+			"denols",
+			"gopls",
+			"lua_ls",
+			"markdown_oxide",
+			"ruff",
+			"rust_analyzer",
+			"tinymist",
+			"yamlls",
+			"zls",
+		})
 
 		vim.lsp.config("*", {
 			capabilities = require("cmp_nvim_lsp").default_capabilities(),
 		})
 
-		-- Server-specific configurations (only non-default settings)
+		--  Non-default LSP settings
 		local custom = {
 			ruff = {
 				settings = {
@@ -38,8 +38,8 @@ return {
 				settings = {
 					basedpyright = {
 						analysis = {
-							disableOrganizeImports = true, -- using ruff
-							typeCheckingMode = "off", -- using ruff
+							disableOrganizeImports = true, -- address conflict ruff
+							typeCheckingMode = "off", -- ditto
 							diagnosticMode = "workspace",
 							useLibraryCodeForTypes = true,
 						},
@@ -47,7 +47,7 @@ return {
 				},
 			},
 
-			-- vim.lsp.config doesn't merge lists, so we do it here
+			-- vim.lsp.config (vim.tbl_deep_extend) doesn't merge lists, so we do it here
 			denols = { filetypes = vim.tbl_extend("keep", { "json", "jsonc" }, vim.lsp.config.denols.filetypes) },
 		}
 
@@ -55,7 +55,6 @@ return {
 			vim.lsp.config(server, config)
 		end
 
-		-- Configure diagnostics
 		local x = vim.diagnostic.severity
 		vim.diagnostic.config({
 			update_in_insert = false,
@@ -64,8 +63,6 @@ return {
 			severity_sort = true,
 			underline = true,
 			float = { border = "single" },
-			-- float = false,
-			-- virtual_lines = { current_line = true, severity = { min = "ERROR" } },
 		})
 	end,
 }
