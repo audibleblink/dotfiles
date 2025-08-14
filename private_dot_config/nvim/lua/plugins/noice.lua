92ab9aa44f28749577e55f5bbc8991a9a44c2b2f
Alex Flores <domain@me.com>
2025-08-13 23:50:38 -0500

    (nvim) noice tweaks

--- private_dot_config/nvim/lua/plugins/noice.lua
+++ private_dot_config/nvim/lua/plugins/noice.lua
@@ -4,7 +4,7 @@ return {
 	opts = {
 		presets = {
 			bottom_search = false, -- use a classic bottom cmdline for search
-			command_palette = true, -- position the cmdline and popupmenu together
+			command_palette = false, -- position the cmdline and popupmenu together
 			long_message_to_split = true, -- long messages will be sent to a split
 			inc_rename = false, -- enables an input dialog for inc-rename.nvim
 			lsp_doc_border = true, -- add a border to hover docs and signature help
@@ -12,20 +12,19 @@ return {
 		lsp = {
 			override = {
 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
+				["vim.lsp.util.stylize_markdown"] = false,
 				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 			},
 		},
 		views = {
 			cmdline_popup = {
+				size = { min_width = 66 },
+				position = { row = "40%" },
 				border = {
 					style = "none",
-					padding = { 1, 1 },
+					padding = { 1, 2 },
 				},
-				filter_options = {},
-				win_options = {
-					winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-				},
+				win_options = { winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder" },
 			},
 		},
 		timeout = 2000,
