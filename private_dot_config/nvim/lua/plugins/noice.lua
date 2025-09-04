853386705e09f796db902097bf76480b920716c7
Alex Flores <domain@me.com>
2025-09-03 23:59:58 -0500

    (nvim) tame hover and sig things

--- private_dot_config/nvim/lua/plugins/noice.lua
+++ private_dot_config/nvim/lua/plugins/noice.lua
@@ -7,14 +7,12 @@ return {
 			command_palette = false, -- position the cmdline and popupmenu together
 			long_message_to_split = true, -- long messages will be sent to a split
 			inc_rename = false, -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = true, -- add a border to hover docs and signature help
+			lsp_doc_border = false, -- add a border to hover docs and signature help
 		},
 		lsp = {
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = false,
-				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-			},
+			signature = { enabled = false },
+			hover = { enabled = false },
+			message = { enabled = false },
 		},
 		views = {
 			cmdline_popup = {
