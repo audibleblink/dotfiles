14126156ee2549718129cef69b274047c8060184
Alex Flores <domain@me.com>
2025-10-02 10:52:51 -0500

    nvim(harpoon): fmt pass

--- private_dot_config/nvim/lua/plugins/harpoon.lua
+++ private_dot_config/nvim/lua/plugins/harpoon.lua
@@ -1,26 +1,32 @@
 return {
-  "ThePrimeagen/harpoon",
-  enabled = false,
-  branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
-  init = function()
-    local harpoon = require("harpoon")
-    harpoon:setup({})
-    vim.keymap.set("n", "<leader>ha",
-      function() harpoon:list():add() end,
-      { desc = "Harpoon Add" }
-    )
-    vim.keymap.set("n", "<leader>ho",
-      function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
-      { desc = "Harpoon Open" }
-    )
+	"ThePrimeagen/harpoon",
+	enabled = false,
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+	init = function()
+		local harpoon = require("harpoon")
+		harpoon:setup({})
+		vim.keymap.set("n", "<leader>ha", function()
+			harpoon:list():add()
+		end, { desc = "Harpoon Add" })
+		vim.keymap.set("n", "<leader>ho", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "Harpoon Open" })
 
-    vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+		vim.keymap.set("n", "<leader>1", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<leader>2", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<leader>3", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<leader>4", function()
+			harpoon:list():select(4)
+		end)
 
-    local harpoon_extensions = require("harpoon.extensions")
-    harpoon:extend(harpoon_extensions.builtins.navigate_with_number())
-  end,
+		local harpoon_extensions = require("harpoon.extensions")
+		harpoon:extend(harpoon_extensions.builtins.navigate_with_number())
+	end,
 }
