254fb779f2869412e91e3c0dc066cf4e33bce6bd
red <domain@me.com>
2025-10-05 15:53:09 -0500

    feat(nvim/ts): extract markdow from treesitter config

---
+++ private_dot_config/nvim/lua/plugins/markdown-render.lua
@@ -0,0 +1,30 @@
+return {
+	"MeanderingProgrammer/render-markdown.nvim", -- Make Markdown buffers look beautiful
+	ft = { "markdown", "codecompanion" },
+	opts = {
+		completions = { lsp = { enabled = true } },
+		render_modes = true, -- Render in ALL modes
+		sign = {
+			enabled = false, -- Turn off in the status column
+		},
+		latex = { enabled = false },
+		overrides = {
+			filetype = {
+				codecompanion = {
+					html = {
+						tag = {
+							buf = { icon = " ", highlight = "CodeCompanionChatIcon" },
+							file = { icon = " ", highlight = "CodeCompanionChatIcon" },
+							group = { icon = " ", highlight = "CodeCompanionChatIcon" },
+							help = { icon = "󰘥 ", highlight = "CodeCompanionChatIcon" },
+							image = { icon = " ", highlight = "CodeCompanionChatIcon" },
+							symbols = { icon = " ", highlight = "CodeCompanionChatIcon" },
+							tool = { icon = "󰯠 ", highlight = "CodeCompanionChatIcon" },
+							url = { icon = "󰌹 ", highlight = "CodeCompanionChatIcon" },
+						},
+					},
+				},
+			},
+		},
+	},
+}
--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,84 +1,53 @@
 return {
-	{
-		"MeanderingProgrammer/render-markdown.nvim", -- Make Markdown buffers look beautiful
-		ft = { "markdown", "codecompanion" },
-		opts = {
-			render_modes = true, -- Render in ALL modes
-			sign = {
-				enabled = false, -- Turn off in the status column
-			},
-			latex = { enabled = false },
-			overrides = {
-				filetype = {
-					codecompanion = {
-						html = {
-							tag = {
-								buf = { icon = " ", highlight = "CodeCompanionChatIcon" },
-								file = { icon = " ", highlight = "CodeCompanionChatIcon" },
-								group = { icon = " ", highlight = "CodeCompanionChatIcon" },
-								help = { icon = "󰘥 ", highlight = "CodeCompanionChatIcon" },
-								image = { icon = " ", highlight = "CodeCompanionChatIcon" },
-								symbols = { icon = " ", highlight = "CodeCompanionChatIcon" },
-								tool = { icon = "󰯠 ", highlight = "CodeCompanionChatIcon" },
-								url = { icon = "󰌹 ", highlight = "CodeCompanionChatIcon" },
-							},
-						},
-					},
-				},
-			},
-		},
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	branch = "main",
+	build = ":TSUpdate",
+	opts = {
+		-- web dev
+		"html",
+		"css",
+		"javascript",
+		"typescript",
+		"tsx",
+		"json",
+		"vue",
+		"svelte",
+
+		-- other
+		"base",
+		"c",
+		"go",
+		"rust",
+		"glsl",
+		"ruby",
+		"typst",
+		"python",
+		"lua",
+		"hcl",
+		"terraform",
+		"markdown",
+		"markdown_inline",
+		"luadoc",
+		"printf",
+		"regex",
+		"vim",
+		"vimdoc",
+		"yaml",
 	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = false,
-		branch = "main",
-		build = ":TSUpdate",
-		opts = {
-			-- web dev
-			"html",
-			"css",
-			"javascript",
-			"typescript",
-			"tsx",
-			"json",
-			"vue",
-			"svelte",
 
-			-- other
-			"base",
-			"c",
-			"go",
-			"rust",
-			"glsl",
-			"ruby",
-			"typst",
-			"python",
-			"lua",
-			"hcl",
-			"terraform",
-			"markdown",
-			"markdown_inline",
-			"luadoc",
-			"printf",
-			"regex",
-			"vim",
-			"vimdoc",
-			"yaml",
-		},
+	config = function(_, opts)
+		require("nvim-treesitter").install(opts)
+		require("nvim-treesitter").setup()
 
-		config = function(_, opts)
-			require("nvim-treesitter").install(opts)
-			require("nvim-treesitter").setup()
-
-			-- Auto load tree-sitter if we have parser
-			vim.api.nvim_create_autocmd("FileType", {
-				desc = "Load tree-sitter for supported file types",
-				pattern = opts,
-				callback = function()
-					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-					vim.treesitter.start()
-				end,
-			})
-		end,
-	},
+		-- Auto load tree-sitter if we have parser
+		vim.api.nvim_create_autocmd("FileType", {
+			desc = "Load tree-sitter for supported file types",
+			pattern = opts,
+			callback = function()
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				vim.treesitter.start()
+			end,
+		})
+	end,
 }
