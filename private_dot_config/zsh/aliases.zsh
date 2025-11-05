023e8187a504db777f416cadaa0cf227ffd20eaf
Alex Flores <domain@me.com>
2025-11-04 22:53:56 -0600

    feat(zsh): replace tv with fzf-tmux
      - for bookmarks, cdjump, and projects
      - remove floats widget

--- private_dot_config/zsh/aliases.zsh
+++ private_dot_config/zsh/aliases.zsh
@@ -38,7 +38,6 @@ alias dots="nvim -c 'Telescope chezmoi find_files'"
 # ]]]
 # Coding [[[
 alias be='bundle exec'
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 # ]]]
 # Git [[[
 alias ga='git add'
@@ -62,11 +61,18 @@ alias fml='e -O $( git diff --name-only | uniq )' # Open all merge conflicts
 
 (( $+commands[tree] )) && alias tree='LS_COLORS="di=34:ln=35:so=32;40:ex=31" tree --charset utf-8'
 
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 export FZF_PREVIEW_COMMAND="bat --style=numbers,changes --wrap never --color always {}"
-export FZF_DEFAULT_OPTS=' --color=bg:-1,bg+:-1
-  --border="none" --border-label="" --preview-window="border-rounded" --padding="1"
-  --margin="1" --prompt="❯ " --marker="+" --pointer="◆"
-  --separator="─" --scrollbar="│" --info="right"'
+export FZF_DEFAULT_OPTS='
+  --color=fg:#d0d0d0,fg+:#bac2de,bg:#1e1e2e,bg+:#1e1e2e
+  --color=hl:#74c7ec,hl+:#5fd7ff,info:#cba6f7,marker:#f38ba8
+  --color=prompt:#eba0ac,spinner:#f9e2af,pointer:#89b4fa,header:#b4befe
+  --color=border:#1e1e2e,separator:#313244,preview-bg:#181825,preview-border:#181825
+  --color=preview-scrollbar:#181825,label:#8edf5f,query:#bac2de,disabled:#3f3d46
+  --border="block" --border-label="" --preview-window="border-sharp" --padding="1,2,1,2"
+  --margin="1,2,1,2" --prompt="  " --marker="" --pointer="❯"
+  --separator="─" --scrollbar="" --info="right" --cycle --ghost Search...'
+# https://vitormv.github.io/fzf-themes#eyJib3JkZXJTdHlsZSI6ImJsb2NrIiwiYm9yZGVyTGFiZWwiOiIiLCJib3JkZXJMYWJlbFBvc2l0aW9uIjoxMCwicHJldmlld0JvcmRlclN0eWxlIjoic2hhcnAiLCJwYWRkaW5nIjoiMSwyLDEsMiIsIm1hcmdpbiI6IjEsMiwxLDIiLCJwcm9tcHQiOiLvgIIgIiwibWFya2VyIjoi75GEIiwicG9pbnRlciI6IuKdryIsInNlcGFyYXRvciI6IuKUgCIsInNjcm9sbGJhciI6IiIsImxheW91dCI6ImRlZmF1bHQiLCJpbmZvIjoicmlnaHQiLCJjb2xvcnMiOiJmZzojZDBkMGQwLGZnKzojYmFjMmRlLGJnOiMxZTFlMmUsYmcrOiMxZTFlMmUsaGw6Izc0YzdlYyxobCs6IzVmZDdmZixpbmZvOiNjYmE2ZjcsbWFya2VyOiNmMzhiYTgscHJvbXB0OiNlYmEwYWMsc3Bpbm5lcjojZjllMmFmLHBvaW50ZXI6Izg5YjRmYSxoZWFkZXI6I2I0YmVmZSxib3JkZXI6IzFlMWUyZSxzZXBhcmF0b3I6IzMxMzI0NCxwcmV2aWV3LWJnOiMxODE4MjUscHJldmlldy1ib3JkZXI6IzE4MTgyNSxwcmV2aWV3LXNjcm9sbGJhcjojMTgxODI1LGxhYmVsOiM4ZWRmNWYscXVlcnk6I2JhYzJkZSxkaXNhYmxlZDojM2YzZDQ2In0=
 
 export PATH=$PATH:$HOME/.cargo/bin
 
